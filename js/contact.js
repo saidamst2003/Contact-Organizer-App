55ba8188b3280cdb013e24c7518ddc30f056d897
mohamed <mohamed@gmail.com>
2025-01-17 17:57:11 +0100

    i fix responsive

--- js/contact.js
+++ js/contact.js
@@ -9,10 +9,28 @@ const display_section = document.getElementById("display-section");
 const empty = document.getElementById("item-empty");
 // empty element in second side
 const display_empty = document.getElementById("display-empty");
+
 // list element to display 
 const img = document.getElementById("card-profile");
 const username = document.getElementById("card-username");
 const phone = document.getElementById("card-phone");
+// display element to display 
+const display_img = document.getElementById("display-profile");
+const display_username = document.getElementById("display-username");
+const display_phone = document.getElementById("display-phone");
+const display_email = document.getElementById("display-email");
+const display_gender = document.getElementById("display-gender");
+const display_adress = document.getElementById("display-adress");
+
+// ajouter element 
+const ajouter_img = document.getElementById("ajouter-profile");
+const ajouter_name = document.getElementById("ajouter-name");
+const ajouter_last = document.getElementById("ajouter-last");
+const ajouter_phone = document.getElementById("ajouter-phone");
+const ajouter_email = document.getElementById("ajouter-email");
+const ajouter_gender = document.getElementById("ajouter-gender");
+const ajouter_adress = document.getElementById("ajouter-adress");
+
 
 let contactlist = [];
 
@@ -40,10 +58,13 @@ if (contactlist.length === 0) {
 }
 
 function ajouter(list) {
-  
+
 
 }
 
 function afficher(contact) {
+  contact.forEach(function(item, index) {
+
+  });
   
 }
